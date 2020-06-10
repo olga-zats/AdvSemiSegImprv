d1d4341f64b7c34347e871c38975691bc085dbe9
Olga Zatsarynna <zatsarinnayaolga@gmail.com>
2020-06-10 22:07:08 +0200

    Remove old scripts

--- ave_pool.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0   
-sizes=(107  321)
-for size in ${sizes[*]}
-do
-   python ave_pool_train.py --snapshot-dir snapshots/ave_pool --partial-data 0.125 --num-steps 20000  --random-seed 0 --kernel-size ${size},${size} --gpu 0
-   python evaluate_voc.py --restore-from snapshots/ave_pool/VOC_20000_${size}_0.pth --save-dir ave_pool --save-name results_${size}_0.txt --gpu 0
-done
--- gl_pyramid.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-seeds=(0)
-for seed in ${seeds[*]}
-do
-   python gl_pyramid_train.py --snapshot-dir snapshots/gl_pyramid --partial-data 0.125 --num-steps 20000  --random-seed $seed  --gpu 0
-   python evaluate_voc.py --restore-from snapshots/gl_pyramid/VOC_20000_${seed}.pth --save-dir gl_pyramid --save-name results_${seed}.txt --gpu 0  
-done
